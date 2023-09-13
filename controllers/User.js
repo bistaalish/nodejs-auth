f8d5cb099f598e7c6e2468668d9b9a3fff912ef0
Alish Bista <bistace321@gmail.com>
2023-09-13 20:23:46 +0545

    feat: created login

--- controllers/User.js
+++ controllers/User.js
@@ -12,9 +12,31 @@ const handleUserSignup = async (req,res) => {
 
 
 
-const handleLogin = (req,res) => {
-    
+const handleLogin = async (req,res) => {
+    const {email,password} = req.body
+    if (!email || !password) {
+        throw new BadRequestError("Please provide email and password")
+    }
+    const user = await User.findOne({email})
+    // if user does not exists
+    if (!user) {
+        throw new UnauthenticatedError("Invalid Credentials")
+    }
+    const isPasswordCorrect = await user.comparePassword(password)
+    // Compare password
+    if (!isPasswordCorrect) {
+        throw new UnauthenticatedError("Invalid Credentials")
+    }
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({
+        user: {
+            name: user.getName()
+        },
+        token
+    }) 
+
 }
+
 
 module.exports  = {
     handleLogin,
--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,7 @@ const express = require('express');
 const {handleLogin,handleUserSignup} = require('../controllers/User');
 const router = express.Router();
 
-router.post("/login",(req,res) => {
-   res.send("login") 
-})
+router.post("/login",handleLogin)
 router.post("/signup",handleUserSignup)
 
 module.exports = router
